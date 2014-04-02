ab7817dea2d2bf92ebf06730b068dd480744a7b9
majidahadi <majid.ahadi09@gmail.com>
2014-04-02 11:35:57 -0600

    update

--- file/file/file.cpp
+++ file/file/file.cpp
@@ -2,13 +2,26 @@
 //
 
 #include "stdafx.h"
+#include "stdafx.h"
+#include "stdio.h"
+#include "conio.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	FILE *infile;
+	char  s[80];
+	int n=80;
+
 	infile = fopen("input.txt", "r");
-	
+	if (infile == NULL) {
+	  printf("invalid file");
+	  getch();
+	  return 0;
+	}
+	//fgets(s,n,infile);
+	printf("file valid");
+	getch();
 
 	fclose(infile);
 	return 0;
